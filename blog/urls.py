eab4b657a477c79293fcfba226891d70df87daf1
Mark Dunne <Markdunne117@gmail.com>
2023-10-31 19:01:39 +0000

    Add function to view posts in detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # 'slug' keyword name matches 'slug' parameter in
+    # get method of PostDetail class in blog/views.py
+    # file. That is how they are linked together.
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -8,7 +8,36 @@ class PostList(generic.ListView):
     # queryset value set to the content of our post model
     # filtered with a status of 1 meaning it is published
     # and ordered by created on in descending order
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
     # limits the number of pages that can appear on one page to 6
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        # gets published post with the correct slug
+        post = get_object_or_404(queryset, slug=slug)
+        # gets comments of post ordered by oldest 1st
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        # if the user id exists to say a user has liked
+        # the post the liked value will be set to True
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        # sending all information to render method
+        return render(
+            # sending request
+            request,
+            # specifying template required
+            'post_detail.html',
+            # using a dictionary to supply our context
+            {
+                'post': post,
+                'comments': comments,
+                'liked': liked
+            },
+        )
