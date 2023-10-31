102ed1401a0879ba6eaa3d53e732063d2f8a4a44
Mark Dunne <Markdunne117@gmail.com>
2023-10-31 16:50:52 +0000

    Build and add funtionality to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Post, Comment
+# summernote library used to style post text
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+# register post model on admin portal
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    # adding more funtionality to the admin portal
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    # prepopulating the slug field based on the title
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    # Allows an admin to approve comments
+    actions = ['approve_comments']
+
+    # function allowing admin to set approved to True
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
